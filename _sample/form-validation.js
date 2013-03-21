1f2e6804da84df5bcca8ab16fb9769c6a8b410f9
her0satr <her0satr@yahoo.com>
2013-03-21 08:08:20 -0700

    Sample

---
+++ _sample/form-validation.js
@@ -0,0 +1,36 @@
+	var frmvalidator = new Validator("quickcontact");
+	frmvalidator.EnableMsgsTogether();
+	frmvalidator.addValidation("name","req","Please enter your name. <br />");
+	frmvalidator.addValidation("email","email","Please make sure you enter a valid email address. <br />"); 
+
+	// custom 1
+	function DoCustomValidation()
+	{
+		var frm = document.forms["quickcontact"];
+		if((frm.email.value.length == '0')&&(frm.telephone.value.length == '0'))
+		{
+			sfm_show_error_msg("You must supply either your telephone number or email address.");
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}	
+	frmvalidator.setAddnlValidationFunction(DoCustomValidation);	
+
+	// hpot
+	function DoCustomValidation2()
+	{
+		var frm = document.forms["quickcontact"];
+		if(frm.nopexyz.value == '')
+		{
+			return true;
+		}
+		else
+		{
+			sfm_show_error_msg("be off with you, spammer!");
+			return false;
+		}
+	}	
+	frmvalidator.setAddnlValidationFunction(DoCustomValidation2);					
---
+++ _sample/script.js
@@ -0,0 +1,29 @@
+/*
+* Author:      Marco Kuiper (http://www.marcofolio.net/)
+*/
+$(document).ready(function()
+{
+	// Set the nescesarry data
+	$("#jquerynav a").each(function(){
+		var backgroundPositions = $(this).css('background-position').split(" "); // Returns "##px" and "##px"
+		$(this).data("originalXpos", backgroundPositions[0].slice(0, -2)); // Retrieve the original X position
+		$(this).data("newYpos", 0); // Set the new Y position to 0
+	});
+	
+	// Capture the "hover" events
+	$("#jquerynav a").hover(function(){
+		$(this)
+			.data("newYpos", $(this).data("newYpos") + 1)
+			.stop()
+			.animate({
+				backgroundPosition: $(this).data("originalXpos") + "px " + $(this).data("newYpos") * 60 + "px"
+			}, 600, "easeOutCirc");
+	}, function(){
+		$(this)
+			.data("newYpos", $(this).data("newYpos") + 1)
+			.stop()
+			.animate({
+				backgroundPosition: $(this).data("originalXpos") + "px " + $(this).data("newYpos") * 60 + "px"
+			}, 300, "easeInCirc");
+	});
+});
