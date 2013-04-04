eda1dc368d825457e1006996ea2663a346acd1ba
her0satr <her0satr@yahoo.com>
2013-04-04 06:15:51 -0700

    Add SEO Title

--- update.sql
+++ update.sql
@@ -21,3 +21,12 @@ INSERT INTO `portofolio` (`portofolio_id`, `portofolio_name`, `portofolio_title`
 (17, '', 'TV Beruang Kaltim', '<p>TV Beruang Kaltim</p>', '', '2013/04/04/20130404_203352_2824.jpg', '2013-03-18'),
 (18, '', 'Berita Jawinews', '<p>Berita Jawinews</p>', '', '2013/04/04/20130404_203425_5426.jpg', '2013-03-17'),
 (19, '', 'BKD Muara Enim', '<p>BKD Muara Enim</p>', '', '2013/04/04/20130404_203452_8586.jpg', '2013-03-16');
+
+INSERT INTO `config` (`config_id`, `config_category`, `config_name`, `config_title`, `config_content`, `config_hide`) VALUES
+(39, 'Payment', 'no 2', 'Transfer BNI', '<p>No Rek : 0132257978</p>\n<p>an: Moch Wahib Dariyadi</p>\n<p>Cab Univ. Negeri Malang</p>', 0),
+(40, 'Payment', 'no 3', 'Transfer Mandiri', '<p>No Rek : 1440010419270</p>\n<p>an: Moch Wahib Dariyadi</p>\n<p>Cab. Wahid Hasyim Malang</p>', 0),
+(41, 'Payment', 'no 4', 'Transfer Paypal', '<p>Account :</p>\n<p>mitradesain@yahoo.com</p>\n<p>Paypal.com</p>', 0),
+(42, '', 'footer-info', 'Footer Info', '<p>Tertarik menggunakan layanan kami dan ingin berkonsultasi lebih lanjut?. Silahkan Menghubungi Kami atau add kami di Facebook.</p>', 0),
+(43, '', 'meta-keyword', 'Meta Keyword', '<p>jasa pembuatan website,pembuatan,website,toko online,company profile,sekolah,iklan,portal</p>', 0),
+(44, '', 'meta-desc', 'Meta Description', '<p>Kami adalah jasa pembuatan website, seperti E-profile, toko online (online store), profile perusahaan (company profile), portal, iklan baris</p>', 0),
+(45, '', 'meta-title', 'Meta Title', '<p>Mitra Desain -</p>', 0);
